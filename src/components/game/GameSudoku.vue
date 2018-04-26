2e0f6da90945ae7281aba34370881d00e2c8de90
PrimoZiv <zpdong93@gmail.com>
2018-04-26 17:34:24 +0800

    Complete sudoku game.

--- src/components/game/GameSudoku.vue
+++ src/components/game/GameSudoku.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="container">
         <div class="sudoku-table" :style="{width: clientSize, height: clientSize}">
-            <div v-for="x in 9" :key="x" class="sudoku-row">
-                <div v-for="y in 9" :key="y" class="sudoku-item"
-                    :class="{'top-line': x === 1,
-                             'bottom-line': x === 9,
-                             'left-line': y === 1,
-                             'right-line': y === 9,
-                             'top-split': x === 4 || x === 7,
-                             'left-split': y === 4 || y === 7,
-                             'selected': x === selected.x && y === selected.y}"
+            <div v-for="(row, x) in matrix" :key="x" class="sudoku-row">
+                <div v-for="(v, y) in row" :key="y" class="sudoku-item"
+                    :class="{'top-line': x === 0,
+                             'bottom-line': x === 8,
+                             'left-line': y === 0,
+                             'right-line': y === 8,
+                             'top-split': x === 3 || x === 6,
+                             'left-split': y === 3 || y === 6,
+                             'selected': x === selected.x && y === selected.y,
+                             'active': getMmatrixMem(x, y) === 0}"
                     @click="selectBox(x, y)">
-                    {{getMmatrix(x, y)}}
+                    {{v || ''}}
                 </div>
             </div>
         </div>
@@ -21,7 +22,7 @@
 </template>
 
 <script>
-import sudoku from 'sudoku-matrix'
+import {blink} from 'sudoku-matrix'
 export default {
     data () {
         return {
@@ -29,9 +30,20 @@ export default {
             menuText: 'Start',
             clientSize: 0,
             selected: {
-                x: 0,
-                y: 0
+                x: -1,
+                y: -1
             },
+            matrixM: [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ],
             matrix: [
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -55,6 +67,8 @@ export default {
         document.onkeyup = (e) => {
             if (e.keyCode === 27) {
                 this.$router.push('/')
+            } else {
+                this.fillNumber(e)
             }
         }
     },
@@ -69,22 +83,96 @@ export default {
         },
         selectBox (x, y) {
             // 如果不是disable的单元格则可以选中
-            this.selected.x = x
-            this.selected.y = y
+            if (this.matrixM[x][y] === 0) {
+                this.selected.x = x
+                this.selected.y = y
+            }
         },
         sudoku () {
             this.menuText = 'Loading'
             setTimeout(() => {
-                let matrix = sudoku()
-                this.matrix.splice(0, this.matrix.length, ...matrix)
+                let m = blink()
+                this.matrixM = m
+                this.matrix.splice(0, this.matrix.length, [...m[0]], [...m[1]], [...m[2]], [...m[3]],
+                        [...m[4]], [...m[5]], [...m[6]], [...m[7]], [...m[8]])
                 this.menuText = 'Start'
                 this.menu = false
             }, 10)
         },
-        getMmatrix (x, y) {
+        getMmatrixMem (x, y) {
             x = +x
             y = +y
-            return this.matrix && this.matrix[x - 1][y - 1]
+            return this.matrixM && this.matrixM[x][y]
+        },
+        fillNumber (e) {
+            let number = 0
+            let code = e.keyCode
+            if (code >= 49 && code <= 57) {
+                number = code - 48
+            } else if (code >= 97 && code <= 105) {
+                number = code - 96
+            }
+
+            if (number && this.selected.x !== -1 && this.selected.y !== -1) {
+                this.$set(this.matrix[this.selected.x], this.selected.y, number)
+                if (this.checkMatrix(this.matrix) === true) {
+                    this.menu = true
+                    this.menuText = 'Success!'
+                }
+            }
+        },
+        checkMatrix (matrix) {
+            // Check row
+            for (let [i, r] of matrix.entries()) {
+                if (r.includes(0)) {
+                    return false
+                } else if ((new Set(r)).size < 9) {
+                    return {
+                        row: i
+                    }
+                }
+            }
+
+            // Check column
+            let count = 9
+            let s = new Set()
+            while (count--) {
+                s.clear()
+                for (let i = 0; i < 9; i++) {
+                    if (!/^[1-9]$/.test(matrix[i][count])) {
+                        return false
+                    }
+                    s.add(matrix[i][count])
+                }
+                if (s.size < 9) {
+                    return {
+                        column: count
+                    }
+                }
+            }
+
+            // Check block
+            for (let x = 0; x < 3; x++) {
+                for (let y = 0; y < 3; y++) {
+                    let m = x * 3
+                    let n = y * 3
+                    let block = [
+                        matrix[m][n], matrix[m][n + 1], matrix[m][n + 2],
+                        matrix[m + 1][n], matrix[m + 1][n + 1], matrix[m + 1][n + 2],
+                        matrix[m + 2][n], matrix[m + 2][n + 1], matrix[m + 2][n + 2]
+                    ]
+
+                    if (block.includes(0)) {
+                        return false
+                    } else if ((new Set(block)).size < 9) {
+                        return {
+                            block: [x, y]
+                        }
+                    }
+                }
+            }
+
+            return true
         }
     }
 }
@@ -103,7 +191,7 @@ $borderColor: rgb(165, 140, 59);
     justify-content: center;
     align-items: center;
 
-    .sudoku-mask {
+    .sudoku-mask, .sudoku-bingo {
         position: absolute;
         top: 0;
         left: 0;
@@ -123,7 +211,7 @@ $borderColor: rgb(165, 140, 59);
         line-height: 100px;
         background: #fff;
         opacity: 1;
-        font-size: 50px;
+        font-size: 40px;
         text-align: center;
         border-radius: 10px;
         cursor: pointer;
@@ -153,14 +241,15 @@ $borderColor: rgb(165, 140, 59);
         display: inline-block;
         border-left: 1px solid $borderColor;
         border-top: 1px solid $borderColor;
-        cursor: pointer;
+        cursor: default;
 
         display: flex;
         justify-content: center;
         align-items: center;
 
-        &.disabled {
-            cursor: default;
+        &.active {
+            cursor: pointer;
+            background: rgb(71, 86, 109);
         }
 
         &.selected {
