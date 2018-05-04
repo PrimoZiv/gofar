61a78b55a4e33bc6bbd19becad588f72d686a074
PrimoZiv <zpdong93@gmail.com>
2018-05-04 17:47:25 +0800

    Init game Match.

--- src/components/game/GameMatch.vue
+++ src/components/game/GameMatch.vue
@@ -1,11 +1,28 @@
 <template>
-    <div class="container">Match</div>
+    <div class="container">
+        <div v-for="x of 6" :key="x - 1" class="match-row">
+            <div v-for="y of 6" :key="(x - 1) * 6 + y - 1" class="match-item"
+                :class="[isActive === icons[(x - 1) * 6 + y - 1] ? 'active' : '', 'el-icon-' + icons[(x - 1) * 6 + y - 1]]">
+                {{icons[(x - 1) * 6 + y - 1] || ''}}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import {shuffle} from 'lodash'
 export default {
     data () {
-        return {}
+        return {
+            types: 18,
+            icons: ['goods', 'news', 'date', 'printer', 'time', 'message', 'bell',
+                'phone', 'service', 'view', 'star-off', 'location-outline', 'zoom-in',
+                'picture-outline', 'edit-outline', 'rank', 'circle-plus-outline', 'search',
+                'goods', 'news', 'date', 'printer', 'time', 'message', 'bell',
+                'phone', 'service', 'view', 'star-off', 'location-outline', 'zoom-in',
+                'picture-outline', 'edit-outline', 'rank', 'circle-plus-outline', 'search'],
+            isActive: false
+        }
     },
     mounted () {
         this.$bus.$emit('full-screen', true)
@@ -17,6 +34,13 @@ export default {
     },
     destroyed () {
         this.$bus.$emit('full-screen', false)
+    },
+    methods: {
+        getClientSize () {
+            let w = document.body.clientWidth
+            let h = document.body.clientHeight
+            return (w > h ? h : w) + 'px'
+        }
     }
 }
 </script>
