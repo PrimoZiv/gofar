80f002379c85d7d3e1231641c830bb6a69940a7e
Primo <zpdong93@gmail.com>
2018-05-05 11:53:58 +0800

    Optimize game Match.

--- src/components/game/GameMatch.vue
+++ src/components/game/GameMatch.vue
@@ -1,20 +1,27 @@
 <template>
     <div class="container">
         <transition-group tag="div" name="cell" class="match-table" :style="{width: clientSize, height: clientSize}">
-            <!-- <div v-for="x of 6" :key="x - 1" class="match-row"> -->
-                <div v-for="(v, id) of icons" :key="id" class="match-item"
-                    @click="turnOver(id)"
-                    :class="[turn.includes(id) ? 'turn' : '']">
+            <div v-for="(v, id) of icons" :key="v.id" class="match-item"
+                @click="turnOver(id)"
+                :class="[turn.includes(id) ? 'turn' : '']">
 
-                    <div class="match-icon"
-                        :class="['el-icon-' + v.name,
-                            turn.includes(id) ? 'turn' : '',
-                            successCells.includes(v.name) ? 'success' : '']">
-                        
-                    </div>
+                <div class="match-icon"
+                    :class="['el-icon-' + v.name,
+                        turn.includes(id) ? 'turn' : '',
+                        successCells.includes(v.name) ? 'success' : '']">
+                    
                 </div>
-            <!-- </div> -->
+            </div>
         </transition-group>
+        <div class="match-mask" v-show="complete"></div>
+        <div class="btn-start" v-show="complete">
+            <div class="tip">
+                <span>按Esc键退出</span>
+            </div>
+            <div>
+                <el-button type="primary" @click="refresh">{{btnText}}</el-button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -31,13 +38,17 @@ export default {
                 'phone', 'service', 'view', 'star-off', 'location-outline', 'zoom-in',
                 'picture-outline', 'edit-outline', 'rank', 'circle-plus-outline', 'search'].map(function (name, index) {
                     return {
+                        id: index,
                         name: name
                     }
                 }),
             isActive: false,
             clientSize: 0,
             turn: [],
-            successCells: []
+            successCells: [],
+            complete: false,
+            btnText: 'Refresh',
+            stepCount: 0
         }
     },
     mounted () {
@@ -46,6 +57,14 @@ export default {
 
         window.onresize = () => {
             this.clientSize = this.getClientSize()
+        }
+
+        document.onkeyup = (e) => {
+            if (e.keyCode === 27) {
+                this.$router.push('/')
+            } else if (e.keyCode === 83) {
+                this.refresh()
+            }
         }
 
         this.refresh()
@@ -63,21 +82,23 @@ export default {
             let m
             let t = this.turn
 
-            if (!t.includes(index)) {
+            this.stepCount++
+
+            if (!t.includes(index) && !this.successCells.includes(this.icons[index].name)) {
                 m = t.push(index)
             }
 
             if (m % 2 === 0) {
-                if (this.icons[t[m - 1]].name === this.icons[t[m - 2]].name) {
-                    this.successCells.push(this.icons[t[m - 1]].name)
-                }
-
                 {
                     let x = t[m - 1]
                     let y = t[m - 2]
                     setTimeout(() => {
                         this.turnSplice(x)
                         this.turnSplice(y)
+                        if (this.icons[x].name === this.icons[y].name && !this.successCells.includes(this.icons[x].name)) {
+                            this.successCells.push(this.icons[x].name)
+                        }
+                        this.checkSuccess()
                     }, 800)
                 }
             }
@@ -89,7 +110,17 @@ export default {
             this.turn.splice(i, 1)
         },
         refresh () {
+            this.complete = false
+            this.turn.splice(0, this.turn.length)
+            this.successCells.splice(0, this.successCells.length)
             this.icons = shuffle(this.icons)
+            this.stepCount = 0
+        },
+        checkSuccess () {
+            if (this.successCells.length === this.types) {
+                this.btnText = 'Success! Use ' + this.stepCount + ' steps.'
+                this.complete = true
+            }
         }
     }
 }
@@ -109,22 +140,42 @@ export default {
     justify-content: center;
     align-items: center;
 
+    .match-mask {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0.5;
+        background: #000;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 50;
+    }
+
+    .btn-start {
+        position: absolute;
+        background: #fff;
+        opacity: 1;
+        font-size: 40px;
+        text-align: center;
+        border-radius: 10px;
+        cursor: pointer;
+        padding: 20px;
+        z-index: 50;
+    }
+
     .match-table {
         width: 100%;
         height: 100%;
+        padding: 20px;
+        box-sizing: border-box;
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
         justify-content: space-around;
     }
-
-    // .match-row {
-    //     width: 100%;
-    //     height: 15%;
-    //     display: flex;
-    //     flex-direction: row;
-    //     justify-content: space-around;
-    // }
 
     .match-item {
         width: 15%;
@@ -142,6 +193,10 @@ export default {
     .match-icon {
         opacity: 0;
     }
+    .tip {
+        font-size: 16px;
+        line-height: 30px;
+    }
 }
 
 .match-item.turn {
@@ -153,6 +208,7 @@ export default {
     transition: all 1s;
 }
 .match-icon.success {
+    transform: rotateY(180deg);
     opacity: 1;
 }
 .cell-move {
